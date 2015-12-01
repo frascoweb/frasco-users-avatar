007ce56c5e18132296621250cde26d211491d91d
maximebf <maxime.bouroumeau@gmail.com>
2015-12-01 18:32:03 +0100

    support for generating first letter avatars

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='frasco-users-avatar',
-    version='0.1',
+    version='0.2',
     url='http://github.com/frascoweb/frasco-users-avatar',
     license='MIT',
     author='Maxime Bouroumeau-Fuseau',
